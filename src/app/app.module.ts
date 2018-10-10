eb68f7c4b2112d9fa87aad266796f2695c679f9a
Sukhpreet Sekhon <sukhpreetsekhon@Sukhpreets-MacBook-Pro.local>
2018-10-09 23:49:25 -0400

    Angular Frontend - display list of users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'user',
+    component: UserComponent,
+    data: {title: 'User List'}
+
+  },
+
+  {
+    path: '',
+    redirectTo: '/user',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes, {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
